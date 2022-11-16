7be978e8c3a2b9a2be473ca4d46401b437d1fd57
donghyeok kim <jeombi@naver.com>
2022-11-16 21:05:25 +0900

    22.11.16 맵 이동 스크립트, 1월드 1스테이지 씬
    맵 이동 스크립트, 1월드 1스테이지 씬

---
+++ NONA/Assets/Scripts/WorldMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMove : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+    }
+}
