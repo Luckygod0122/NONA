547e2529b48495ecefb2ebb46207f87b343f055c
donghyeok kim <jeombi@naver.com>
2022-12-14 23:45:18 +0900

    12.14
    점프, 스킬 버튼 - 누른 상태, 비활성화 상태 구현
    
    맵 속도 9로 변경 - 너무 빨라서 눈 아파서

--- NONA/Assets/Scripts/Player/PlayerUIManager.cs
+++ NONA/Assets/Scripts/Player/PlayerUIManager.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class PlayerUIManager : MonoBehaviour
 {
     public GameObject PlayerScript;
+    public GameObject Jump_Down;
+    public GameObject Skill_Button;
+    public GameObject Skill_Down;
+    public GameObject Skill_False;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +24,26 @@ public class PlayerUIManager : MonoBehaviour
     public void JumpClick()
     {
         PlayerScript.GetComponent<PlayerController>().Jump();
+        Jump_Down.gameObject.SetActive(true);
+        Invoke("Jump_Down_False", 0.1f);
+
     }
     public void SkillClick()
     {
         PlayerScript.GetComponent<PlayerController>().DashSkill();
+        Skill_Down.gameObject.SetActive(true);
+        Invoke("Skill_END", 2f);
+    }   
+
+    public void Jump_Down_False()
+    {
+        Jump_Down.gameObject.SetActive(false);
+    }
+
+    public void Skill_END()
+    {
+        Skill_Button.gameObject.SetActive(false);
+        Skill_Down.gameObject.SetActive(false);
+        Skill_False.gameObject.SetActive(true);
     }
 }
