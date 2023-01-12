d201e6fbccafdde27cb693407f5572df09f124b4
donghyeok kim <jeombi@naver.com>
2023-01-12 17:40:04 +0900

    23.01.12
    코인 스크립트 수정

--- NONA/Assets/Scripts/Obstacle/Part.cs
+++ NONA/Assets/Scripts/Obstacle/Part.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Part : MonoBehaviour
 {
     public GameObject PointCon;
+    private AudioSource CoinSound;
+    private Animator animator;
 
     void Start()
     {
-        
+        CoinSound = GetComponent<AudioSource>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     
@@ -21,8 +24,10 @@ public class Part : MonoBehaviour
     { 
         if (collision.CompareTag("Player")) // 부딪힌 것의 태그가 Player일 경우 Part 오브젝트 삭제
         {
+            CoinSound.Play();
+            animator.SetTrigger("Part_E");
             PointCon.GetComponent<PointController>().GetPartion();
-            Destroy(this.gameObject, 0.1f); // 부품 접촉시 0.1초 뒤에 삭제
+            Destroy(this.gameObject, 0.2f); // 부품 접촉시 0.2초 뒤에 삭제
         }
     }
 
