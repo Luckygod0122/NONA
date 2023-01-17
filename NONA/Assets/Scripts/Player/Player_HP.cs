cb15bb640c76107b6fae3dcc9254c745dba9511c
choimingi11 <100996850+choimingi11@users.noreply.github.com>
2023-01-17 16:40:18 +0900

    2023/1/17 작업일지
    자잘한 것 지움

--- NONA/Assets/Scripts/Player/Player_HP.cs
+++ NONA/Assets/Scripts/Player/Player_HP.cs
@@ -6,7 +6,6 @@ public class Player_HP : MonoBehaviour
 {
     public int PlayerHP = 1; // 플레이어 시작 체력 1
     public int part = 0; // 플레이어가 시작할 때 먹은 부품의 갯수 
-    public int Score = 0; // 부품을 먹었을 때 얻는 점수
 
     void Update()
     {
@@ -26,10 +25,8 @@ public class Player_HP : MonoBehaviour
         else if (collision.CompareTag("Part")) // 부딪힌 것의 태그가 Part일 경우
         {
             part += 1;   // part 1개씩 추가
-            Score += 10;
             if (part == 5) // 만약 part가 5개라면 플레이어 체력 +1
             {
-                part = 0;
                 PlayerHP += 1;
             }
         }
