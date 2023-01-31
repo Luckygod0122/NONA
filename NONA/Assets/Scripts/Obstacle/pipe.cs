dd497377d5c9b0d0243f1131f846040f2d6aff45
choimingi11 <100996850+choimingi11@users.noreply.github.com>
2023-01-31 17:11:44 +0900

    2023/1/31 작업일지
    2월드 1스테이지 장애물 조정 및 특수 장애물 애니메이션 (애니메이션은 아직 미완성)
    / 2월드 1스테이지 UI 적용

---
+++ NONA/Assets/Scripts/Obstacle/pipe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipe : MonoBehaviour
+{
+    Animator anim;
+    GameObject Player;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Player.GetComponent<PlayerController>().dashSkill == true && collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetTrigger("destroy");
+            Destroy(this, 0.3f);
+        }
+    }
+}
