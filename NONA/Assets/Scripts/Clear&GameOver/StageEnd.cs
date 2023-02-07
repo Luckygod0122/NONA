da61cdc7d0f452e1e0b0f8e4c738de54f00679c7
bloodsilk0 <100887534+bloodsilk-jwk@users.noreply.github.com>
2023-02-07 16:54:08 +0900

    23.02.07
    스테이지 클리어 후 애니메이션 나타내기 (완료)

--- NONA/Assets/Scripts/Clear&GameOver/StageEnd.cs
+++ NONA/Assets/Scripts/Clear&GameOver/StageEnd.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Animations;
+
 
 public class StageEnd : MonoBehaviour
 {
-    public int ResultCase;
+    public int ResultNumber;
 
+    public GameObject Result;
     public Animator ResultAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        ResultAnimator = GetComponent<Animator>();
         PlayerPrefs.SetInt("CurrentStage", SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void Update()
+    {
+        ShowResult();
     }
 
     public void RestartButton()
@@ -29,20 +34,24 @@ public class StageEnd : MonoBehaviour
 
     public void ShowResult()
     {
-        switch(ResultCase)
+        switch(ResultNumber)
         {
             case 0:
                 break;
             case 1:
+                Result.SetActive(true);
                 ResultAnimator.SetInteger("ResultCase", 1);
                 break;
             case 2:
+                Result.SetActive(true);
                 ResultAnimator.SetInteger("ResultCase", 2);
                 break;
             case 3:
+                Result.SetActive(true);
                 ResultAnimator.SetInteger("ResultCase", 3);
                 break;
             case 4:
+                Result.SetActive(true);
                 ResultAnimator.SetBool("IfFail", true);
                 break;
         }
