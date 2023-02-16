ee756cf2403b42edd47d57e130c3b87d2879b6f7
choimingi11 <100996850+choimingi11@users.noreply.github.com>
2023-02-16 14:35:26 +0900

    2023/2/16
    플레이어 체력이 닳았을 때 잠깐동안 무적 판정 + 오류 수정

--- NONA/Assets/Scripts/Player/Player_HP.cs
+++ NONA/Assets/Scripts/Player/Player_HP.cs
@@ -9,6 +9,7 @@ public class Player_HP : MonoBehaviour
 
     public int PlayerHP = 1; // 플레이어 시작 체력 1
     public int part = 0; // 플레이어가 시작할 때 먹은 부품의 갯수 
+    public int count = 0;
 
     public AudioSource mysfx;
     public AudioClip ReviveSound;
@@ -35,7 +36,19 @@ public class Player_HP : MonoBehaviour
     {
         if (collision.CompareTag("obstacle")) // 부딪힌 것의 태그가 obstacle일 경우
         {
-            PlayerHP -= 1; // 플레이어 체력 -1
+            if (count == 0)
+            {
+                PlayerHP -= 1; // 플레이어 체력 -1
+                count = 1;
+            }
+            
+            else if (count == 1)
+            {
+                PlayerHP -= 0; // 무적 판정
+                Invoke("inv", 3);
+                
+            }
+            
         }
         else if (collision.CompareTag("Part")) // 부딪힌 것의 태그가 Part일 경우
         {
@@ -55,4 +68,10 @@ public class Player_HP : MonoBehaviour
         anim.SetBool("Die",true);
        mysfx.PlayOneShot(DieSound);
     }
+
+    private void inv() //무적 판정 함수
+    {
+        count = 0;
+    }
+
 }
