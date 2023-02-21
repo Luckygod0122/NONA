96d9b34644e671e4d162644ee1e322a2fdfea111
donghyeok kim <jeombi@naver.com>
2023-02-21 17:50:07 +0900

    23.02.21
    플레이어 장애물 접촉 시, 무적 시간 0.6초, 빨간색으로 깜빡깜빡 거리게 색상 변경

--- NONA/Assets/Scripts/Player/Player_HP.cs
+++ NONA/Assets/Scripts/Player/Player_HP.cs
@@ -7,7 +7,7 @@ public class Player_HP : MonoBehaviour
     public GameObject worldmove;
     public Animator anim;
 
-    public int PlayerHP = 1; // 플레이어 시작 체력 1
+    public int PlayerHP = 3; // 플레이어 시작 체력 1
     public int part = 0; // 플레이어가 시작할 때 먹은 부품의 갯수 
     public int count = 0;
 
@@ -19,9 +19,13 @@ public class Player_HP : MonoBehaviour
 
     public StageEnd EndScript;
 
+    private SpriteRenderer Sprite; // 플레이어 체력 감소할 때 빨간색으로 색 변경 위해서 작성
+    private Color originColor; // 원래 색깔
+
     private void Start()
     {
-
+        Sprite = GetComponent<SpriteRenderer>();
+        originColor = Sprite.material.color;
     }
     void Update()
     {
@@ -46,16 +50,14 @@ public class Player_HP : MonoBehaviour
                 PlayerHP -= 1; // 플레이어 체력 -1
                 count = 1;
                 mysfx.PlayOneShot(HitSound);
-
+                StartCoroutine(OnHitColor());
             }
-            
+
             else if (count == 1)
             {
-                PlayerHP -= 0; // 무적 판정
-                Invoke("inv", 3);
-                
+                StartCoroutine(unbeatable());
+                // count = 0;
             }
-            
         }
         else if (collision.CompareTag("Part")) // 부딪힌 것의 태그가 Part일 경우
         {
@@ -68,6 +70,30 @@ public class Player_HP : MonoBehaviour
         }
     }
 
+    // 플레이어 장애물 접촉 시, 무적 상태를 색으로 표시
+    private IEnumerator OnHitColor()
+    {
+        Sprite.material.color = Color.red; // 빨간색으로 변경
+        yield return new WaitForSeconds(0.1f); // 바로 윗줄 0.1초 대기
+        Sprite.material.color = originColor;
+        yield return new WaitForSeconds(0.1f);
+        Sprite.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        Sprite.material.color = originColor;
+        yield return new WaitForSeconds(0.1f);
+        Sprite.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        Sprite.material.color = originColor;
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private IEnumerator unbeatable() // 0.6초 무적
+    {
+        PlayerHP -= 0;
+        yield return new WaitForSeconds(0.6f);
+        count = 0;
+    }
+
     private void Dead() // 플레이어 사망 시 부서짐
     {
 //        Destroy(this.gameObject, 1f);
@@ -75,10 +101,4 @@ public class Player_HP : MonoBehaviour
         anim.SetBool("Die",true);
        mysfx.PlayOneShot(DieSound);
     }
-
-    private void inv() //무적 판정 함수
-    {
-        count = 0;
-    }
-
 }
